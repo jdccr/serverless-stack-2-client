aa6054bb9d0f0e95c08f55299434319565c6ecbf
jdccr <jdccr@hotmail.com>
2019-03-15 16:05:17 -0600

    Connecting the billing form

--- src/config.js
+++ src/config.js
@@ -39,20 +39,7 @@ const config = process.env.REACT_APP_STAGE === 'prod'
   ? prod
   : dev;
 
-export default {
+export default {  
   MAX_ATTACHMENT_SIZE: 5000000,
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
-  },
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
-  },
-  cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
-  }
+  ...config
 };
